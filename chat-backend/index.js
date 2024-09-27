a176e6bc0c8b03be89f2217696593d330b7d446b
Sai Chinmay Tripurari <47316906+saichinmay2000@users.noreply.github.com>
2024-09-27 15:28:12 +0530

    final version

---
+++ chat-backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const http = require('http')
+const {Server} = require('socket.io')
+
+const app = express();
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log("User Connected!",socket.id);
+    socket.on('sendMessage', (message) => {
+        io.emit('receiveMessage', message)
+    })
+    socket.on('disconnect', () => {
+        console.log("User Disconnected!");
+    })
+})
+
+server.listen(4000, () => {
+    console.log("Server started on port 4000");
+})
